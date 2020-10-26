03b0f7bf25c58514c0ac2d7a0eedf5252ab6a715
Aswin Vayiravan <aswin@deducely.com>
2020-10-26 19:44:19 -0400

    works?

--- get-fx-rate/entrypoint.sh
+++ get-fx-rate/entrypoint.sh
@@ -36,5 +36,5 @@
 
 sh -c "echo The Base Currency is $INPUT_BASE_CURRENCY"
 #API_RESPONSE=$(curl https://api.exchangeratesapi.io/latest?base=$INPUT_BASE_CURRENCY\&symbols=INR)
-sh -c curl https://api.exchangeratesapi.io/latest?base=$INPUT_BASE_CURRENCY\&symbols=INR
+curl https://api.exchangeratesapi.io/latest?base=$INPUT_BASE_CURRENCY\&symbols=INR
 #sh -c "echo $API_RESPONSE"
