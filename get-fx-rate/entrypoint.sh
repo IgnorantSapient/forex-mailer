48717f7e6d62304617700a2b863325ce8735563e
Aswin Vayiravan <aswin@deducely.com>
2020-10-26 14:25:32 -0400

    added exchange rate API

--- get-fx-rate/entrypoint.sh
+++ get-fx-rate/entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/sh -l
 
 sh -c "echo The Base Currency is $INPUT_BASE_CURRENCY"
+API_RESPONSE=$(curl https://api.exchangeratesapi.io/latest?base=$INPUT_BASE_CURRENCY\&symbols=INR)
+#sh -c "curl https://api.exchangeratesapi.io/latest?base=$INPUT_BASE_CURRENCY\&symbols=INR"
+sh -c "echo $API_RESPONSE"
