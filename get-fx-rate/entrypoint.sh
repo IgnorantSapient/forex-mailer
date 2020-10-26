0118e7c5e9d35a92507b3f5c669acbfae2bbc0c0
Aswin Vayiravan <aswin@deducely.com>
2020-10-26 16:22:42 -0400

    this code will print exchange rate

--- get-fx-rate/entrypoint.sh
+++ get-fx-rate/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -l
 
+#Executing with sh -c helps in catching errors inside Github Actions,
+#If one runs commnds without it, errors arent spit out.
 sh -c "echo The Base Currency is $INPUT_BASE_CURRENCY"
-#API_RESPONSE=$(curl https://api.exchangeratesapi.io/latest?base=$INPUT_BASE_CURRENCY\&symbols=INR)
-sh -c "curl https://api.exchangeratesapi.io/latest?base=$INPUT_BASE_CURRENCY\&symbols=INR"
-#sh -c "echo $API_RESPONSE"
+
+sh -c "curl https://api.exchangeratesapi.io/latest?base=$INPUT_BASE_CURRENCY\&symbols=INR > resp.json"
+sh -c "cat resp.json | jq '.rates.INR' > fxRate.json"
+sh -c 'FX_RATE=`cat fxRate.json`;echo 1 CAD = $FX_RATE INR'
