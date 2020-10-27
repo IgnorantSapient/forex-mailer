3b172afcbd8be4e43a04a3b5efb4d245c42e3506
Aswin Vayiravan <aswin@deducely.com>
2020-10-26 20:12:13 -0400

    ...

--- get-fx-rate/entrypoint.sh
+++ get-fx-rate/entrypoint.sh
@@ -38,7 +38,5 @@ apt-get install -y jq
 #apt-get install -y curl
 echo The Base Currency is $INPUT_BASE_CURRENCY
 curl https://api.exchangeratesapi.io/latest?base=$INPUT_BASE_CURRENCY\&symbols=INR
-
-echo $API_RESPONSE
 #curl https://api.exchangeratesapi.io/latest?base=$INPUT_BASE_CURRENCY&symbols=INR
 #sh -c "echo $API_RESPONSE"
