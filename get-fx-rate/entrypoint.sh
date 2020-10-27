be05505cb48ac13dddb9e3a23c44823fb98fe351
Aswin Vayiravan <aswin@deducely.com>
2020-10-26 20:29:17 -0400

    final if bugs dont exist

--- get-fx-rate/entrypoint.sh
+++ get-fx-rate/entrypoint.sh
@@ -2,6 +2,8 @@
 apk add jq
 apk add curl
 echo The Base Currency is $INPUT_BASE_CURRENCY
-API_RESPONSE=$(curl https://api.exchangeratesapi.io/latest?base=$INPUT_BASE_CURRENCY\&symbols=INR)
+echo The Target Currency is $INPUT_TARGET_CURRENCY
+
+API_RESPONSE=$(curl https://api.exchangeratesapi.io/latest?base=$INPUT_BASE_CURRENCY\&symbols=$INPUT_TARGET_CURRENCY)
 FX_RATE=$(echo $API_RESPONSE | jq '.rates.INR')
-echo 1 $INPUT_BASE_CURRENCY = $FX_RATE INR
+echo 1 $INPUT_BASE_CURRENCY = $FX_RATE $INPUT_TARGET_CURRENCY
