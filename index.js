afb6622fbbfa8584ae5f7589a2e5fa6613ae01fe
Aswin Vayiravan <aswin@deducely.com>
2020-11-08 21:50:55 -0500

    added github context for JSON webhook payload

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ const setResultsToGithubOutput = (response) => {
 };
 
 try {
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
   getExchangeRate().then((response) => setResultsToGithubOutput(response));
 } catch (error) {
   core.setFailed(error.message);
