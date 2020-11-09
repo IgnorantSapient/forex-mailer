3f90bbbfa7d2c3e82983a7a5abe19b0db50c0a2d
Aswin Vayiravan <aswin@deducely.com>
2020-11-08 21:54:45 -0500

    added explaining comments

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const fetch = require("node-fetch");
 
-const baseCurrency = core.getInput('base-currency');
-const targetCurrency = core.getInput('target-currency');
+const baseCurrency = core.getInput("base-currency");
+const targetCurrency = core.getInput("target-currency");
 
 const getExchangeRate = async () => {
   const response = await fetch(
@@ -13,6 +13,9 @@ const getExchangeRate = async () => {
   return data;
 };
 
+/*This function is used to set the value of the exchange-rate variable
+ so that other github-actions steps and jobs can access it.
+ */
 const setResultsToGithubOutput = (response) => {
   const exchangeRate = parseFloat(response.rates[targetCurrency]);
   console.log(response);
@@ -21,7 +24,7 @@ const setResultsToGithubOutput = (response) => {
 
 try {
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
   getExchangeRate().then((response) => setResultsToGithubOutput(response));
 } catch (error) {
